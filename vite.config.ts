9c8f5a62411c656927bc0d3e186499d5e1c4be7b
João Victor <jvmazagao@gmail.com>
2025-05-18 14:22:02 -0300

    try to fix route 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-  base: '/j_mazagao/',
+  base: '/j_mazagao',
   plugins: [
     react(),
     mdx(),
   ],
   resolve: {
     alias: { '@': '/src' },
-  },
+  }
 });
