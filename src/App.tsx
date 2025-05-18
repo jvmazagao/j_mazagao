6282d5a5d36ee1f62a293f56c8deed5feed9a95e
João Victor <jvmazagao@gmail.com>
2025-05-18 14:38:54 -0300

    try to fix route with hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './presentation/pages/home-page'
 import BlogPage from './presentation/pages/blog-page'
 import ProjectsPage from './presentation/pages/projects-page'
