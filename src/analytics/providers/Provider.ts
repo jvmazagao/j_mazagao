a583718c257c93d3e162d30fc1f6037611d98c59
João Victor <jvmazagao@gmail.com>
2025-06-09 09:54:15 -0300

    refactor: change interface to be abstract class to reuse logic

--- src/analytics/providers/Provider.ts
+++ src/analytics/providers/Provider.ts
@@ -1,9 +1,32 @@
 import type { EventParameters } from "../events";
 
-export interface Provider {
-  isReady: () => boolean;
-  trackEvent: (eventName: string, parameters: EventParameters) => void;
-  trackPageView: (element: string, location: string) => void;
-  trackClick: (element: string, location: string) => void;
-  initialize: () => void;
+export abstract class Provider {
+  constructor() {
+    Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+      .filter(name => name !== 'constructor' && typeof this[name as keyof this] === 'function')
+      .forEach(name => {
+        const key = name as keyof this;
+        this[key] = (this[key] as any).bind(this);
+      });
+  }
+
+  abstract isReady(): boolean;
+  
+  abstract trackEvent(eventName: string, parameters: EventParameters): void;
+
+  abstract initialize(): void;
+
+  trackPageView(title: string, location: string) {
+    this.trackEvent("page_view", {
+      page_title: title,
+      page_location: location,
+    });
+  }
+
+  trackClick(element: string, location: string) {
+    this.trackEvent("button_click", {
+      button_name: element,
+      page_location: location,
+    });
+  }
 }
--- src/analytics/providers/firebase/Firebase.ts
+++ src/analytics/providers/firebase/Firebase.ts
@@ -1,23 +1,15 @@
 import { isSupported, getAnalytics, type Analytics, logEvent } from "firebase/analytics";
 import { initializeApp, type FirebaseApp } from "firebase/app";
 import { firebaseConfig } from "@/analytics/providers/firebase/config";
-import type { Provider } from "@/analytics/providers/Provider";
+import { Provider } from "@/analytics/providers/Provider";
 import type { EventParameters } from "@/analytics/events";
 
-export class FirebaseAnalyticsProvider implements Provider {
-  private instance: Analytics | null;
+export class FirebaseAnalyticsProvider extends Provider {
+  private instance: Analytics | null = null;
   private app: FirebaseApp;
-  private _isInitializing: boolean = false;
-  private _initializationPromise: Promise<boolean> | null = null;
 
   constructor() {
-     Object.getOwnPropertyNames(Object.getPrototypeOf(this))
-    .filter(name => name !== 'constructor' && typeof this[name as keyof this] === 'function')
-    .forEach(name => {
-      const key = name as keyof this;
-      this[key] = (this[key] as any).bind(this);
-    });
-    this.instance = null;
+    super();
     try {
       this.app = initializeApp(firebaseConfig);
     } catch (error) {
@@ -27,12 +19,12 @@ export class FirebaseAnalyticsProvider implements Provider {
   }
 
   isReady() {
-    return this.instance !== null && !this._isInitializing;
+    return this.instance !== null;
   }
 
   trackEvent(eventName: string, parameters: EventParameters) {
     try {
-      if (this?.isReady() && this.instance) {
+      if (this.isReady() && this.instance) {
         logEvent(this.instance, eventName, parameters);
       } else {
         console.warn("Analytics not ready, skipping event:", eventName);
@@ -42,47 +34,20 @@ export class FirebaseAnalyticsProvider implements Provider {
     }
   }
 
-  trackPageView(title: string, location: string) {
-    this.trackEvent("page_view", {
-      page_title: title,
-      page_location: location,
-    });
-  }
-
-  trackClick(element: string, location: string) {
-    this.trackEvent("button_click", {
-      button_name: element,
-      page_location: location,
-    });
-  }
-
   async initialize() {
-    if (this._initializationPromise) {
-      return this._initializationPromise;
-    }
-
-    this._isInitializing = true;
-    this._initializationPromise = (async () => {
-      try {
-        const supported = await isSupported();
-
-        if (!supported) {
-          console.warn("Firebase Analytics not supported");
-          this._isInitializing = false;
-          return false;
-        }
-
-        this.instance = getAnalytics(this.app); // getAnalytics(app)
-        console.log("Firebase Analytics initialized successfully");
-        return true;
-      } catch (error) {
-        console.error("Firebase Analytics initialization error:", error);
+    try {
+      const supported = await isSupported();
+      if (!supported) {
+        console.warn("Firebase Analytics not supported");
         return false;
-      } finally {
-        this._isInitializing = false;
       }
-    })();
 
-    return this._initializationPromise;
+      this.instance = getAnalytics(this.app);
+      console.log("Firebase Analytics initialized successfully");
+      return true;
+    } catch (error) {
+      console.error("Firebase Analytics initialization error:", error);
+      return false;
+    }
   }
 }
